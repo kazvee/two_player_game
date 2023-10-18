c188f0d04998a2382ee382cf73d8c82825effc77
kazvee <kayvee123@gmail.com>
2023-10-18 00:57:37 -0600

    Refactor: Players take turns & scores are displayed after every round

--- main.rb
+++ main.rb
@@ -3,25 +3,37 @@ require_relative 'player'
 
 class Main
   def initialize
-    @question = Question.new("Player 1")
     @player1 = Player.new("Player 1")
     @player2 = Player.new("Player 2")
+    @current_player = @player1
   end
 
-  def display_lives_remaining(player)
-    puts "#{player.name} has #{player.lives_remaining} lives remaining. 🧮"
+  def display_lives_remaining
+    puts "🧮 Current Score: Player 1 has #{@player1.lives_remaining}/3 & Player 2 has #{@player2.lives_remaining}/3 🧮"
+  end
+
+  def switch_player
+    @current_player = (@current_player == @player1) ? @player2 : @player1
   end
 
   def play_game
-    while @player1.lives_remaining > 0
-      @question.ask_question
+    while @player1.lives_remaining > 0 && @player2.lives_remaining > 0
+      puts "✨ New Turn ✨"
+      question = Question.new(@current_player.name)
+      question.ask_question
       player_guess = gets.chomp
+      question.check_answer(player_guess, @current_player)
+      display_lives_remaining
 
-      @question.check_answer(player_guess, @player1)
-      display_lives_remaining(@player1)
+      if @player1.lives_remaining <= 0 || @player2.lives_remaining <= 0
+        break
+      end
+      
+      switch_player
     end
 
-    puts "Game Over for #{@player1.name}! ❌ 😭"
+    winner = @player1.lives_remaining > 0 ? @player1 : @player2
+    puts "Game Over! #{winner.name} wins with a score of #{winner.lives_remaining}/3! 🏆"
   end
 end
 
